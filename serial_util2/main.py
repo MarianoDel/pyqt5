2ef0cbd4ef0c540ceaa1791a858f7b0af14d53f9
Slackware Live User <live@darkstar.example.net>
2018-01-29 08:56:32 +0000

    mejoro serialcomm

---
+++ serial_util2/main.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog
+from ui_serial import Ui_Dialog
+from serialcomm import SerialComm
+
+class Dialog(QDialog):
+    def __init__(self):
+        super(Dialog, self).__init__()
+
+        # Set up the user interface from Designer.
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        # # Make some local modifications.
+        # self.ui.colorDepthCombo.addItem("2 colors (1 bit per pixel)")
+        #
+        # # Connect up the buttons.
+        self.ui.enviar1.clicked.connect(self.Envio1)
+        self.ui.enviar2.clicked.connect(self.Envio2)
+        self.ui.enviar3.clicked.connect(self.Envio3)
+
+    # def __show__(self):
+        self.s = SerialComm(self.MyObjCallBack, '/dev/ttyACM0')
+        if self.s.port_open == False:
+            self.ui.diag.setText("Sin puerto serie!!!")
+            sys.exit(-1)
+        else:
+            self.ui.diag.setText("puerto serie abierto OK!")
+
+
+    def Envio1(self, event):
+        self.ui.recibido.setText("env 1")
+        self.s.Write("boton 1\n")
+
+    def Envio2(self, event):
+        self.ui.recibido.setText("env 2")
+        self.s.Write("boton 2\n")
+
+    def Envio3(self, event):
+        self.ui.recibido.setText("env 3")
+        self.s.Write("boton 3\n")
+
+    def MyObjCallBack (self, dataread):
+        print ("callback called!")
+        d = dataread[:-1]
+        self.ui.recibido.setText(d)
+
+    #capturo el cierre
+    def closeEvent (self, event):
+        self.s.Close()
+        event.accept()
+
+
+
+
+
+app = QApplication(sys.argv)
+w = Dialog()
+w.show()
+sys.exit(app.exec_())
---
+++ serial_util2/ui_serial.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'serial.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(533, 395)
+        self.enviar1 = QtWidgets.QPushButton(Dialog)
+        self.enviar1.setGeometry(QtCore.QRect(60, 30, 211, 34))
+        self.enviar1.setObjectName("enviar1")
+        self.enviar2 = QtWidgets.QPushButton(Dialog)
+        self.enviar2.setGeometry(QtCore.QRect(60, 100, 211, 34))
+        self.enviar2.setObjectName("enviar2")
+        self.enviar3 = QtWidgets.QPushButton(Dialog)
+        self.enviar3.setGeometry(QtCore.QRect(60, 170, 211, 34))
+        self.enviar3.setObjectName("enviar3")
+        self.recibido = QtWidgets.QLabel(Dialog)
+        self.recibido.setGeometry(QtCore.QRect(60, 240, 421, 41))
+        font = QtGui.QFont()
+        font.setFamily("Noto Sans [GOOG]")
+        font.setPointSize(14)
+        self.recibido.setFont(font)
+        self.recibido.setObjectName("recibido")
+        self.diag = QtWidgets.QLabel(Dialog)
+        self.diag.setGeometry(QtCore.QRect(60, 330, 421, 41))
+        font = QtGui.QFont()
+        font.setFamily("Noto Sans [GOOG]")
+        font.setPointSize(14)
+        self.diag.setFont(font)
+        self.diag.setText("")
+        self.diag.setObjectName("diag")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.enviar1.setText(_translate("Dialog", "Presione para enviar"))
+        self.enviar2.setText(_translate("Dialog", "Presione para enviar"))
+        self.enviar3.setText(_translate("Dialog", "Presione para enviar"))
+        self.recibido.setText(_translate("Dialog", "Nada en Rx"))
+
