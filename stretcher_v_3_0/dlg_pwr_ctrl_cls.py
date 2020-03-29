e372aaaeeed7f3c135833cdf2098846c96322a97
MED <marianodeleu@yahoo.com.ar>
2020-03-29 18:59:12 -0300

    algo mas a pwr ctrl

--- stretcher_v_3_0/dlg_pwr_ctrl_cls.py
+++ stretcher_v_3_0/dlg_pwr_ctrl_cls.py
@@ -17,49 +17,127 @@ class PowerControlDialog(QDialog):
         self.ui = Ui_PowerControlDialog()
         self.ui.setupUi(self)
 
-        # get the close event
+        # get the close event and connect the other buttons
         self.ui.doneButton.clicked.connect(self.accept)
+        self.ui.triangUpButton.clicked.connect(self.triangUpBtn)
+        self.ui.triangDwnButton.clicked.connect(self.triangDwnBtn)
+        self.ui.squareUpButton.clicked.connect(self.squareUpBtn)
+        self.ui.squareDwnButton.clicked.connect(self.squareDwnBtn)
+        self.ui.sinusUpButton.clicked.connect(self.sinusUpBtn)
+        self.ui.sinusDwnButton.clicked.connect(self.sinusDwnBtn)
 
-    #     self.ui.UpButton.clicked.connect(self.UpBtn)
-    #     self.ui.DwnButton.clicked.connect(self.DwnBtn)
+        ## this ones must be getted from config.txt file or passed as object arguments
+        self.trianglimit = 100
+        self.squarelimit = 100
+        self.sinuslimit = 100
+        self.peak = 3.6
+        self.resistance065 = 42
+        self.resistance080 = 21
+        self.tempcoef065 = 2.2
+        self.tempcoef080 = 1.8
+        self.tempamb = 25
+        
 
-    #     self.ui.dayButton.clicked.connect(self.ChangeFocusDay)
-    #     self.ui.monthButton.clicked.connect(self.ChangeFocusMonth)
-    #     self.ui.yearButton.clicked.connect(self.ChangeFocusYear)
-    #     self.ui.hourButton.clicked.connect(self.ChangeFocusHour)
-    #     self.ui.minuteButton.clicked.connect(self.ChangeFocusMinute)
-    #     self.new_focus = "DAY"
+        ## first run
+        self.ui.triangLimitLabel.setText(str(self.trianglimit) + "%")
+        self.ui.squareLimitLabel.setText(str(self.squarelimit) + "%")
+        self.ui.sinusLimitLabel.setText(str(self.sinuslimit) + "%")
+        self.UpdateTriang()
+        self.UpdateSquare()
+        self.UpdateSinus()
+        
+
+    def triangUpBtn (self):
+        if self.trianglimit < 100:
+            self.trianglimit += 1
+
+        self.ui.triangLimitLabel.setText(str(self.trianglimit) + "%")
+        self.UpdateTriang()
+
+    def triangDwnBtn (self):
+        if self.trianglimit > 10:
+            self.trianglimit -= 1
+
+        self.ui.triangLimitLabel.setText(str(self.trianglimit) + "%")
+        self.UpdateTriang()
+
+    def squareUpBtn (self):
+        if self.squarelimit < 100:
+            self.squarelimit += 1
+
+        self.ui.squareLimitLabel.setText(str(self.squarelimit) + "%")
+        self.UpdateSquare()
+
+    def squareDwnBtn (self):
+        if self.squarelimit > 10:
+            self.squarelimit -= 1
+
+        self.ui.squareLimitLabel.setText(str(self.squarelimit) + "%")
+        self.UpdateSquare()
+
+    def sinusUpBtn (self):
+        if self.sinuslimit < 100:
+            self.sinuslimit += 1
+
+        self.ui.sinusLimitLabel.setText(str(self.sinuslimit) + "%")
+        self.UpdateSinus()
+
+    def sinusDwnBtn (self):
+        if self.sinuslimit > 10:
+            self.sinuslimit -= 1
+
+        self.ui.sinusLimitLabel.setText(str(self.sinuslimit) + "%")
+        self.UpdateSinus()        
 
 
-    # def UpBtn (self, event=None):
-    #     day = int(self.ui.dayButton.text())
-    #     month = int(self.ui.monthButton.text())
-    #     year = int(self.ui.yearButton.text())
-    #     hour = int(self.ui.hourButton.text())
-    #     minute = int(self.ui.minuteButton.text())
+    def UpdateTriang (self):
+        i = 0.01 * self.peak * self.trianglimit
+        i = i * 0.4083
+        self.ui.triangCurrLabel.setText("{0:.3f}".format(i))
 
-    #     if self.new_focus == "DAY":
-    #         if day < 31:
-    #             day += 1
+        p065 = i * i * self.resistance065
+        p080 = i * i * self.resistance080        
+        self.ui.triangPwr065Label.setText("{0:.1f}".format(p065))
+        self.ui.triangPwr080Label.setText("{0:.1f}".format(p080))
 
-    #     if self.new_focus == "MONTH":
-    #         if month < 12:
-    #             month += 1
+        t065 = p065 * self.tempcoef065 + self.tempamb
+        t080 = p080 * self.tempcoef080 + self.tempamb
+        self.ui.triangTemp065Label.setText(str(int(t065)))
+        self.ui.triangTemp080Label.setText(str(int(t080)))
+        
 
-    #     if self.new_focus == "YEAR":
-    #         if year < 99:
-    #             year += 1
-                
-    #     if self.new_focus == "HOUR":
-    #         if hour < 23:
-    #             hour += 1
+    def UpdateSquare (self):
+        i = 0.01 * self.peak * self.squarelimit
+        i = i * 0.7071
+        self.ui.squareCurrLabel.setText("{0:.3f}".format(i))
 
-    #     if self.new_focus == "MINUTE":
-    #         if minute < 59:
-    #             minute += 1
-                
-    #     self.UpdateNumbers(day, month, year, hour, minute)
+        p065 = i * i * self.resistance065
+        p080 = i * i * self.resistance080        
+        self.ui.squarePwr065Label.setText("{0:.1f}".format(p065))
+        self.ui.squarePwr080Label.setText("{0:.1f}".format(p080))
 
+        t065 = p065 * self.tempcoef065 + self.tempamb
+        t080 = p080 * self.tempcoef080 + self.tempamb
+        self.ui.squareTemp065Label.setText(str(int(t065)))
+        self.ui.squareTemp080Label.setText(str(int(t080)))
+        
+
+    def UpdateSinus (self):
+        i = 0.01 * self.peak * self.sinuslimit
+        i = i * 0.5
+        self.ui.sinusCurrLabel.setText("{0:.3f}".format(i))
+
+        p065 = i * i * self.resistance065
+        p080 = i * i * self.resistance080
+        self.ui.sinusPwr065Label.setText("{0:.1f}".format(p065))
+        self.ui.sinusPwr080Label.setText("{0:.1f}".format(p080))
+
+        t065 = p065 * self.tempcoef065 + self.tempamb 
+        t080 = p080 * self.tempcoef080 + self.tempamb
+        self.ui.sinusTemp065Label.setText(str(int(t065)))
+        self.ui.sinusTemp080Label.setText(str(int(t080)))
+        
+        
         
     # def DwnBtn (self, event=None):
     #     day = int(self.ui.dayButton.text())
